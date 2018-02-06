7e9bd5c3f0054a3d88555b45f2c5de21fb49c6c1
Castano <JuanDiego.Castano@student.tdsb.on.ca>
2018-02-06 13:20:53 -0500

    This is the second program thing

---
+++ MyDesigns.java
@@ -0,0 +1,21 @@
+// The "MyDesigns" class.
+
+// Juan-Diego Castano 
+// Mr Rosen 
+// Tuesday,Febuary 5, 2018
+//    
+
+import java.awt.*;
+import hsa.Console;
+
+public class MyDesigns
+{
+    static Console c;           // The output console
+    
+    public static void main (String[] args)
+    {
+	c = new Console ();
+	
+	// Place your program here.  'c' is the output console
+    } // main method
+} // MyDesigns class
