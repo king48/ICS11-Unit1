76ac2890bc50acbaf8ce9d29f0975d6b60a85251
Castano <JuanDiego.Castano@student.tdsb.on.ca>
2018-02-07 14:38:15 -0500

    this is the second assignment

--- MyText.java
+++ MyText.java
@@ -1,8 +1,8 @@
 // The "MyText" class.
-// Juan-Diego Castano 
-// Mr rosen 
+// Juan-Diego Castano
+// Mr Rosen
 // Tuesday,Febuary 5, 2018
-// This program will look exactly like what is given to us on the sheet for the first   
+// This program will look exactly like what is given to us on the sheet for the first
 import java.awt.*;
 import hsa.Console;
 // The "My Text" class
@@ -14,55 +14,12 @@ public class MyText
     {
 	c = new Console ();
     }
-    
-    public void output () // output  
+
+
+    public void output ()  // output
     {
-	c.print (' ', 36);
-	c.println ("Designs");
-
-	c.print (' ', 39);
-	c.println ("D");
-
-	c.print (' ', 38);
-	c.println ("III");
-
-	c.print (' ', 37);
-	c.println ("AAAAA");
-
-	c.print (' ', 36);
-	c.println ("MMMMMMM");
-
-	c.print (' ', 37);
-	c.println ("OOOOO");
-
-	c.print (' ', 38);
-	c.println ("NNN");
-
-	c.print (' ', 39);
-	c.println ("D");
-
-	c.println (" ");
-
-	c.print (' ', 33);
-	c.println ("PARALLELO");
-
-	c.print (' ', 34);
-	c.print ("A");
-	c.print (' ', 7);
-	c.println ("G");
-
-	c.print (' ', 35);
-	c.print ("R");
-	c.print (' ', 7);
-	c.println ("R");
-
-	c.print (' ', 36);
-	c.print ("A");
-	c.print (' ', 7);
-	c.println ("A");
-
-	c.print (' ', 37);
-	c.println ("LLELOGRAM");
+	c.println ("This is an interger " + 45);
+	c.println ("Two intergers " + 15 + " " + 45);
     }
 
 
