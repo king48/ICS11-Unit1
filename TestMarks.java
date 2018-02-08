b46b06d3887e2a2445667d96a5c93e42cd099794
Castano <JuanDiego.Castano@student.tdsb.on.ca>
2018-02-08 13:44:55 -0500

    This is the newer version of the assignment

--- TestMarks.java
+++ TestMarks.java
@@ -1,7 +1,7 @@
 /* Juan-Diego Castano
    Mr Rosen
    Tuesday,Febuary 5, 2018
-
+program des. goes here
 */
 
 import java.awt.*;
@@ -9,58 +9,52 @@ import hsa.Console;
 
 public class TestMarks
 {
-    public TestMarks ()
+    Console c = new Console ();           // The output console, and class constructor
+    //  TestMarks t = new TestMarks ();
+
+
+    public void titles ()   // This is the output
     {
-	Console c;           // The output console
+	c.print (' ', 16);
+	c.print ("Student", 15);
+	c.print ("Test 1", 13);
+	c.print ("Test 2", 13);
+	c.println ("Test Average");
+	c.println ("");
+
     }
 
 
-    public void Titles ()  // This is the output
+    public void displayMarks ()
     {
-	c.print ("Student", 20);
-	c.print ("Test 1", 43);
-	//c.print ("Test 2", );
-	// c.print ("Test 3", );
-	//   c.print ("Test Average", );
+
+
+	c.print (' ', 16);
+	c.print ("Donald", 15);
+	c.print (45);
+	c.print (80, 13);
+	c.println ((80 + 45) / 2.0, 16, 2);
+
+	c.print (' ', 16);
+	c.print ("Mickey", 15);
+	c.print (33);
+	c.print (39, 13);
+	c.println ((33 + 39) / 2.0, 1);
     }
 
 
-    /*
-	public void line1 ()  // This is the output
-	{
-	    //  c.print ("Donald", );
-	    //c.print (45, );
-	    //  c.print (80, );
-	    //   c.print ((45 + 80) / 2);
 
-	}
-
-
-	public void line2 ()  // This is the output
-	{
-
-	}
-
-
-	public void line3 ()  // This is the output
-	{
-
-	}
-
-    */
-    public TestMarks ()
-    {
-	c = new Console ();
-    }
 
 
     public static void main (String[] args)
     {
-	TestMarks t;
-	t = new TestMarks ();
-	t.Titles ();
+	TestMarks t = new TestMarks ();
+	t.displayMarks ();
+    }
 
 
-	// Place your program here.  'c' is the output console
-    } // main method
-} // TestMakrs class
+    // Place your program here.  'c' is the output console
+} // main method
+
+
+
